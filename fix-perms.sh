31560e8ace461ad44a25670137d3293db9256465
Alejandro Cruz <alu0101424545@ull.edu.es>
2025-11-19 17:13:00 +0000

    prueba permisos

--- fix-perms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Ajustando permisos para que docker pueda acceder a grafana-storage..."
-sudo chown -R 472:472 ./grafana-storage
-sudo chmod -R 775 ./grafana-storage
-echo "Listo."
