2b1e57ce3b8fc7b9666f85f4840aadc67279efcf
Alejandro <alu0101424545@ull.edu.es>
2025-11-19 16:28:07 +0000

    grafana db prueba

--- src/components/pages/weather/Weather.tsx
+++ src/components/pages/weather/Weather.tsx
@@ -4,8 +4,8 @@ import "./Weather.css";
 
 // Lista de estaciones de Open Weather Map con sus URLs de dashboards públicos de Grafana
 const stations = [
-  { id: 1, name: "La Laguna Station", url: "http://localhost:3000/goto/bf4ird9sftrlse?orgId=1" },
-  { id: 2, name: "Observatorio del Teide Station", url: "http://localhost:3000/goto/ff4iuh8thejnkd?orgId=1" },
+  { id: 1, name: "La Laguna Station", url: "http://localhost:3000/public-dashboards/4c22d6cf9b314e23ba231ae392cce972" },
+  { id: 2, name: "Observatorio del Teide Station", url: "http://localhost:3000/public-dashboards/ee99e8bdf3cb4ebfa7ff388d0048b401" },
   { id: 3, name: "Observatorio del Roque de los Muchachos Station", url: "http://localhost:3000/public-dashboards/c09afef2e9394038ba024e59e875023f" },
 ];
 
