c939409193e0d24906d35adec2a00400cd0d7e4d
Alejandro Cruz <alu0101424545@ull.edu.es>
2025-11-19 16:17:43 +0000

    grafana link prueba

--- src/components/pages/weather/Weather.tsx
+++ src/components/pages/weather/Weather.tsx
@@ -6,7 +6,7 @@ import "./Weather.css";
 const stations = [
   { id: 1, name: "La Laguna Station", url: "http://localhost:3000/goto/bf4ird9sftrlse?orgId=1" },
   { id: 2, name: "Observatorio del Teide Station", url: "http://localhost:3000/goto/ff4iuh8thejnkd?orgId=1" },
-  { id: 3, name: "Observatorio del Roque de los Muchachos Station", url: "http://localhost:3000/goto/ef4iug6fal7nka?orgId=1" },
+  { id: 3, name: "Observatorio del Roque de los Muchachos Station", url: "http://localhost:3000/public-dashboards/c09afef2e9394038ba024e59e875023f" },
 ];
 
 // Lista de las futuras estaciones TEIDESAT con sus URLs de dashboards públicos de Grafana
